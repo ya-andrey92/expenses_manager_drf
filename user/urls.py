6d50683a7bd786c4995d2e95cb15b95971c547dd
Andrey Yablonski <ya.andrey92@gmail.com>
2022-11-15 17:17:56 +0300

    add signal to registration

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CustomUserViewSet
 
 urlpatterns = [
-    path('', CustomUserViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('me/', CustomUserViewSet.as_view({'get': 'me', 'put': 'me', 'patch': 'me'})),
+    path('', CustomUserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user'),
+    path('me/', CustomUserViewSet.as_view({'get': 'me', 'put': 'me', 'patch': 'me'}), name='user-detail'),
 ]
