b01793b4a77e5b2ec6d1c3bc212d4a54ba60b344
Andrey Yablonski <ya.andrey92@gmail.com>
2022-11-15 14:41:44 +0300

    Docker added
    - setting docker and docker-compose
    - install library
    - setting db

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
+    'djoser',
+    'timestamps',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +84,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': env.int('POSTGRES_PORT'),
     }
 }
 
